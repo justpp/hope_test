c660611c46d4b17fe9dc73816c98597f2ed24e3f
justpp <youyelan9527@gmail.com>
2019-03-17 22:31:11 +0800

    update .gitignore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
                 </div>
 
                 <div class="links">
-                   <h1>凑居居！啦啦啦啦~</h1>
+                   <h1>凑居居！啦啦啦啦~~</h1>
                 </div>
             </div>
         </div>
