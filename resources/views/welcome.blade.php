5ca69e96b8ffb474a1a423460e36cfe8f3523af4
justpp <youyelan9527@gmail.com>
2019-03-17 23:22:08 +0800

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
         
             <div class="content">
                 <div class="title m-b-md">
-                    test ~
+                    test ~  jj
                 </div>
 
                 <div class="links">
