f8ebf86b5041bc6bae1cfc8d5172ba8fc946d810
YL <1195455487@qq.com>
2019-03-18 09:34:41 +0800

    1

--- app/Http/Controllers/DeploymentController.php
+++ app/Http/Controllers/DeploymentController.php
@@ -20,16 +20,21 @@ class DeploymentController extends Controller
             "cd /usr/www/hope_test",
             "git pull"
         ];
-        $signature = $request->Header('X-hub-Signature');
-        $payload = file_get_contents('php://input');
-        if ($this->isFromGithub($payload,$signature)){
-            foreach ($commands as $command){
-                shell_exec($command);
-            }
-            http_response_code(200);
-        } else {
-            abort(403);
-        }
+      try{
+          $signature = $request->Header('X-hub-Signature');
+          $payload = file_get_contents('php://input');
+          if ($this->isFromGithub($payload,$signature)){
+              foreach ($commands as $command){
+                  shell_exec($command);
+              }
+              http_response_code(200);
+          } else {
+              abort(403);
+          }
+      } catch (\Exception $e){
+          file_put_contents('/usr/www/github.txt', $e . ' ', FILE_APPEND);
+      }
+
     }
 
     /**
