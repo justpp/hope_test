12252c455bee66e0a4c864772fa53ca2930778bd
YL <1195455487@qq.com>
2019-03-18 09:38:27 +0800

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
                 </div>
 
                 <div class="links">
-                   <h1>居居！~</h1>
+                   <h1>居居！~~</h1>
                 </div>
             </div>
         </div>
