e4ac3fabb780596b96b12ce8e1f2dd1807326950
justpp <1195455487@qq.com>
2019-03-22 15:41:21 +0800

    register github route

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,10 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
         ],
+        'github' => [
+            'throttle:60,1',
+            'bindings',
+        ],
     ];
 
     /**
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,6 +39,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
+        $this->mapGithubRoutes();
         //
     }
 
@@ -69,5 +70,13 @@ class RouteServiceProvider extends ServiceProvider
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
+    }
+
+    protected function mapGithubRoutes()
+    {
+        Route::prefix('github')
+            ->middleware('github')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/github.php'));
     }
 }
