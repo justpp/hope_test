9ce032df9c554b6ff5a1e3bb06697b974e64ff73
justpp <1195455487@qq.com>
2019-03-22 18:17:19 +0800

    log3

--- app/Http/Controllers/DeploymentController.php
+++ app/Http/Controllers/DeploymentController.php
@@ -31,13 +31,12 @@ class DeploymentController extends Controller
               http_response_code(200);
               touch('/usr/www/github.txt');
               datefmt_set_timezone('PRC');
-              file_put_contents('/usr/www/github.txt', Carbon::now(). 'pull'."\n", FILE_APPEND);
+              file_put_contents('/usr/www/github.txt', "\n".Carbon::now(). 'pull', FILE_APPEND);
           } else {
               abort(403);
           }
       } catch (\Exception $e){
-          // test
-          file_put_contents('/usr/www/github.txt', $e . "\n", FILE_APPEND);
+          file_put_contents('/usr/www/github.txt',  "\n".$e , FILE_APPEND);
       }
 
     }
